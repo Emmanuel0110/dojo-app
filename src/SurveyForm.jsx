8e91143186533c024e06a283b104ec92f36effe6
Emmanuel Paatz <emmanuel.paatz@gmail.com>
2023-10-31 08:18:46 +0100

    change vote algo

--- src/SurveyForm.jsx
+++ src/SurveyForm.jsx
@@ -5,7 +5,7 @@ const maxNumberOfPoints = numberOfOptions => Math.floor(numberOfOptions/2);
 
 function SurveyForm() {
   const [numberOfOptions, setNumberOfOptions] = useState(0);
-  const [choices, setChoices] = useState({}); //example : {A:4, C:3}
+  const [choices, setChoices] = useState([]); //example : {A:4, C:3}
   const {surveyId} = useParams();
   const navigate = useNavigate();
 
@@ -15,6 +15,7 @@ function SurveyForm() {
       headers: {'Content-Type':'application/json'},
      }).then(response => response.json()).then(({numberOptions}) => {
       if(typeof(numberOptions) === "number") setNumberOfOptions(numberOptions);
+      setChoices(new Array(Math.floor(numberOptions/2)).fill(0));
      });
   },[]);
 
@@ -23,17 +24,31 @@ function SurveyForm() {
     fetch('/vote', {
      method: 'post',
      headers: {'Content-Type':'application/json'},
-     body: JSON.stringify({surveyId , vote: choices})
+     body: JSON.stringify({surveyId , vote: createDict(choices)})
     });
     navigate("/done");
    };
+
+  const createDict = (arr) => {
+    return arr.reduce((acc, currentValue, index) => {
+      console.log(acc, currentValue, index);
+      return {...acc, [currentValue]: arr.length - index};
+    }, {});
+  } 
 
   return <div id="surveyForm">
     <form onSubmit={handleSubmit}>
     {Array.from({length: maxNumberOfPoints(numberOfOptions)},(_, index) => index).map((lineNumber) => {
       return <div>
         <input onChange={e => {
-          setChoices({...choices, [e.currentTarget.value] : maxNumberOfPoints(numberOfOptions) - lineNumber});
+          //setChoices({...choices, [e.currentTarget.value] : maxNumberOfPoints(numberOfOptions) - lineNumber});
+          setChoices(choices.map((el, index) => {
+            if (index === lineNumber) {
+              return e.currentTarget.value;
+            } else {
+              return el;
+            }
+          }))
           }} placeholder={"Choice " + (lineNumber + 1).toString()} type="text"/><br />   
       </div>;
     })}
