ceef3e4e1fc724350aba83e218b8cfc340dd36f1
Emmanuel Paatz <emmanuel.paatz@gmail.com>
2024-01-06 12:45:19 +0100

    add github link (+code formatting)

--- frontend/src/Done.jsx
+++ frontend/src/Done.jsx
@@ -1,8 +1,17 @@
 function Done() {
-  return <div id="done">
-    <div>Thank you !</div>
-    <div id="doneSubtext">You can now close this tab</div>
-  </div>;
+  return (
+    <div id="donePage">
+      <div id="done">
+        <div>Thank you !</div>
+        <div id="doneSubtext">You can now close this tab</div>
+      </div>
+      <div id="githubIconArea">
+        <a href="https://github.com/Emmanuel0110/dojo-app">
+          <div className="githubIcon"></div>
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Done;
--- frontend/src/SurveyPage.jsx
+++ frontend/src/SurveyPage.jsx
@@ -11,14 +11,14 @@ function SurveyPage() {
   const [qrCodeVisible, setQrCodeVisible] = useState(false);
 
   // TODO : try SSE
-  // useEffect(() => {  
-  //   const sse = new EventSource(baseUrl + 'events');  
+  // useEffect(() => {
+  //   const sse = new EventSource(baseUrl + 'events');
   //   function getRealtimeData(numberVotes) {
   //     setResults(results => ({...results, numberVotes}));
-  //   }  
+  //   }
   //   sse.onmessage = e => getRealtimeData(JSON.parse(e.data));
   //   sse.onerror = e => {
-  //     console.log(e);    
+  //     console.log(e);
   //     sse.close();
   //   }
   //   return () => {
@@ -35,14 +35,14 @@ function SurveyPage() {
     let myInterval;
     if (qrCodeVisible) {
       myInterval = setInterval(async () => {
-        const response = await fetch(baseUrl + 'events?surveyId=' + surveyId);
-        const {numberVotes} = await response.json();
-        setResults(results => ({...results, numberVotes}));
+        const response = await fetch(baseUrl + "events?surveyId=" + surveyId);
+        const { numberVotes } = await response.json();
+        setResults((results) => ({ ...results, numberVotes }));
       }, 2000);
     } else {
       clearInterval(myInterval);
     }
-  }, [qrCodeVisible]);
+  }, [qrCodeVisible, surveyId]);
 
   const generateQRcode = () => {
     if (parseInt(numberOfOptions) >= 2) {
@@ -54,7 +54,7 @@ function SurveyPage() {
         .then((response) => response.text())
         .then((id) => {
           if (typeof id === "string") {
-            setSurveyId(id)
+            setSurveyId(id);
             setQrCodeVisible(true);
           }
         });
@@ -62,7 +62,7 @@ function SurveyPage() {
   };
 
   const seeResultsOrQrCode = () => {
-    setQrCodeVisible(qrCodeVisible => !qrCodeVisible);
+    setQrCodeVisible((qrCodeVisible) => !qrCodeVisible);
     fetch(baseUrl + "results?surveyId=" + surveyId, {
       method: "get",
       headers: { "Content-Type": "application/json" },
@@ -74,7 +74,7 @@ function SurveyPage() {
   return (
     <div id="surveyPage">
       <div id="subjectList" className="white-rounded-corners">
-      <textarea name="textarea" placeholder="Copy the vote options here..."></textarea>
+        <textarea name="textarea" placeholder="Copy the vote options here..."></textarea>
       </div>
       <div id="qrCodeArea" className="white-rounded-corners">
         {surveyId === "" ? (
@@ -92,7 +92,10 @@ function SurveyPage() {
         ) : (
           <div>
             <div id="qrCode">
-              <QRCode style={{width: "100%", height: qrCodeVisible ? "100%": 0}} value={window.location.href + (window.location.href.endsWith("/") ? "" : "/") + surveyId} />
+              <QRCode
+                style={{ width: "100%", height: qrCodeVisible ? "100%" : 0 }}
+                value={window.location.href + (window.location.href.endsWith("/") ? "" : "/") + surveyId}
+              />
             </div>
             <br />
             {process.env.NODE_ENV === "development" &&
@@ -102,34 +105,40 @@ function SurveyPage() {
             </Button>
           </div>
         )}
-        {typeof results.numberVotes === "number" && results.numberVotes != 0 && <div>
-          <br/>
+        {typeof results.numberVotes === "number" && results.numberVotes !== 0 && (
+          <div>
+            <br />
             <div>{"Number of votes : " + results.numberVotes}</div>
-          <br/>
-        </div>}
+            <br />
+          </div>
+        )}
         <div>
           <table>
             {results.votes &&
-              Object.entries(results.votes)
-                .map((entry, index) => (
-                  <tr key={index}>
-                    <td style={{ width: "20px" }}>{entry[0]}</td>
-                    <td>
-                      <div
-                        style={{
-                          backgroundColor: "grey",
-                          width: `${entry[1] * (200 / Math.max(...Object.values(results.votes)))}px`,
-                          height: "30px",
-                          color: "white",
-                        }}
-                      >
-                        {entry[1]}
-                      </div>
-                    </td>
-                  </tr>
-                ))}
+              Object.entries(results.votes).map((entry, index) => (
+                <tr key={index}>
+                  <td style={{ width: "20px" }}>{entry[0]}</td>
+                  <td>
+                    <div
+                      style={{
+                        backgroundColor: "grey",
+                        width: `${entry[1] * (200 / Math.max(...Object.values(results.votes)))}px`,
+                        height: "30px",
+                        color: "white",
+                      }}
+                    >
+                      {entry[1]}
+                    </div>
+                  </td>
+                </tr>
+              ))}
           </table>
         </div>
+      </div>
+      <div id="githubIconArea">
+        <a href="https://github.com/Emmanuel0110/dojo-app">
+          <div className="githubIcon"></div>
+        </a>
       </div>
     </div>
   );
